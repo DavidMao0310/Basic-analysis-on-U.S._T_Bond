858f5708bb79dd8f6b3dd0a35d412af2242c44bc
DavidMao <69669042+DavidMao0310@users.noreply.github.com>
2021-01-01 23:04:22 +0800

    Update Time series predict.py

--- Time series predict.py
+++ Time series predict.py
@@ -1,30 +1,41 @@
 import pandas as pd
-import numpy as np
+from pandas.plotting import lag_plot
 import matplotlib.pyplot as plt
-from datetime import datetime
+import seaborn as sns
 import formula as mao
 
 sp = pd.read_csv("adj USTB-10 years.csv")
+sns.boxplot(x=None, y='Adj Close', data=sp, color='lightgreen')
+plt.show()
+print(sp['Close'].sort_values())
+sp = sp.query('Close < 186.938004')
 sp['Date'] = pd.to_datetime(sp['Date'], format='%Y/%m/%d')
-#sp.set_index('Date', inplace=True)
-data = sp['Close']
+sp.set_index('Date', inplace=True)
+data = sp['Adj Close']
+#Can Arima?
+mao.autocorr(data)
+
 #Stationary?
 mao.look(data)
+print('ADF Test \n')
 mao.ADF(data)
 
 diff = mao.diff1(data)
+print('ADF Test \n')
 mao.ADF(diff)
+print('White Noise check \n')
 mao.WN(diff)
-
+print('look order \n')
 mao.look_order(diff)
 #mao.auto_order(diff,2,2)
 #Choose (1,1)
-modelfit = mao.arima_build(data,(1,1,1))
+
+modelfit = mao.arima_buildfit(data,(1,1,1))
 print(modelfit.summary())
 residuals = modelfit.resid
+
 mao.tsdisplay(residuals)
 mao.predict(modelfit,data)
-
 
 
 
