5fa3a88eaf9e89343588f3ca565010c796a391f2
DavidMao <69669042+DavidMao0310@users.noreply.github.com>
2021-01-01 23:03:35 +0800

    Update formula.py

--- formula.py
+++ formula.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_style('darkgrid')
+sns.set_style('whitegrid')
+from pandas.plotting import lag_plot
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.stats.diagnostic import acorr_ljungbox
 from statsmodels.graphics.tsaplots import plot_acf
@@ -11,6 +12,11 @@ from statsmodels.graphics.gofplots import qqplot
 from statsmodels.tsa.stattools import arma_order_select_ic
 from statsmodels.tsa.arima_model import ARIMA
 
+def autocorr(x):
+    plt.figure()
+    lag_plot(x, lag=1, alpha=0.2, c='cornflowerblue')
+    plt.title('Autocorrelation plot with lag = 1')
+    plt.show()
 
 #Stationary?
 def look(x):
@@ -31,42 +37,48 @@ def ADF(x):
     output['value']['Critical Value(10%)'] = t[4]['10%']
     print(output)
 #We need bulid stationary series
+
 def diff1(x):
     t = x.diff(1).dropna()
     return t
 #Since P-Value small, we have enough evidence to say stationary
 #White Noise test
+
 def WN(x):
     wn_pvalue = acorr_ljungbox(x, lags=1, return_df=True)
     print("White Noise Test", wn_pvalue)
 #Since P-Value small, we have enough evidence to say non-WN
-#ARMA order
+
+#To select ARIMA order
 def look_order(t):
     plot_acf(t, lags=20)
     plot_pacf(t, lags=20)
     plt.show()
-def auto_order(x,ar,ma):
+
+def auto_order(x,ar,ma):   #max_ar, max_ma
     z = arma_order_select_ic(x, max_ar=ar, max_ma=ma, ic='aic')
     print(z)
+
 #Build Model
-def arima_build(x, order):
+def arima_buildfit(x, order):
     model = ARIMA(x, order=order)
     t = model.fit()
     return t
+
 #Analysis residuals
-def tsdisplay(y, figsize = (14, 8), lags = 20):
+def tsdisplay(y, figsize = (14, 8), lags = 10):
     tmp_data = pd.Series(y)
     fig = plt.figure(figsize = figsize)
     #Plot the time series
-    tmp_data.plot(ax = fig.add_subplot(311), title = "Time Series of Residuals", legend = False)
+    tmp_data.plot(ax = fig.add_subplot(311), title = "Time Series of Residuals", legend = False, c='seagreen')
     #Plot the ACF:
     plot_acf(tmp_data, lags = lags, zero = False, ax = fig.add_subplot(323))
     plt.xticks(np.arange(1,  lags + 1, 1.0))
     #Plot the PACF:
-    plot_pacf(tmp_data, lags = 20, zero = False, ax = fig.add_subplot(324))
+    plot_pacf(tmp_data, lags = lags, zero = False, ax = fig.add_subplot(324))
     plt.xticks(np.arange(1,  lags + 1, 1.0))
     #Plot the QQ plot of the data:
-    qqplot(tmp_data, line='s', ax = fig.add_subplot(325))
+    qqplot(tmp_data, line='s', ax = fig.add_subplot(325), c='cornflowerblue')
     plt.title("QQ Plot")
     #Plot the residual histogram:
     fig.add_subplot(326).hist(tmp_data, bins = 40, density=True, range=[-5,5])
@@ -76,9 +88,10 @@ def tsdisplay(y, figsize = (14, 8), lags = 20):
     plt.show()
 
 def predict(fit, oridata):
-    fig = plt.figure(figsize=(14, 8))
-    fit.plot_predict(end=oridata.size + 50 , ax=fig.add_subplot(211))
+    fig = plt.figure(figsize=(12, 8))
+    data_size = oridata.shape[0]
+    fit.plot_predict(1, data_size-1 , ax=fig.add_subplot(211), alpha=0.05, plot_insample=True)
     plt.legend().remove()
-    fit.plot_predict(start=oridata.size - 100, end=oridata.size + 50, ax=fig.add_subplot(212))
+    fit.plot_predict(data_size-30, data_size+20, ax=fig.add_subplot(212))
     plt.tight_layout()
     plt.show()
